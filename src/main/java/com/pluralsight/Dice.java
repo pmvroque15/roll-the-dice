1b00fc2a82e5bbc1b914adc4a6c94635cec30b06
pmvroque15 <pmvroque@gmail.com>
2026-04-15 03:08:43 -0400

    Finished exercise

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -2,23 +2,36 @@ package com.pluralsight;
 
 public class Dice {
     static void main(String[] args) {
-        System.out.print(roll(1,6));
         //Declare variables
         int roll1 = 0;
         int roll2 = 0;
 
-        int counterNumber2 = 0;
-        int counterNumber4 = 0;
-        int counterNumber6 = 0;
-        int counterNumber7 = 0;
+        int twoCounter = 0;
+        int fourCounter = 0;
+        int sixCounter = 0;
+        int sevenCounter = 0;
         //Instance of dice
         Dice dice = new Dice();
 
         //loops 100 times
         for (int i = 0; i < 100; i++) {
-            roll1 = roll();
-            roll2 = roll();
+            roll1 = dice.roll();
+            roll2 = dice.roll();
+            int sumOfTwoRolls = (roll1 + roll2);
+            System.out.print("Roll 1: " + roll1 + " - " + roll2 + " Sum: " + sumOfTwoRolls + "\n");
+
+            //if the sum of roll1 and roll2 is 2, and if so, increment the twoCounter
+            if (sumOfTwoRolls == 2) {
+                twoCounter++;
+            } else if (sumOfTwoRolls == 4) {
+                fourCounter++;
+            } else if (sumOfTwoRolls == 6) {
+                sixCounter++;
+            } else if (sumOfTwoRolls == 7) {
+                sevenCounter++;
+            }
         }
+        System.out.printf("TwoCounter: %s\nFourCounter: %s\nSixCounter: %s\nSevenCounter: %s\n", twoCounter,fourCounter,sixCounter,sevenCounter);
     }
     //method roll()
     static int roll() {
