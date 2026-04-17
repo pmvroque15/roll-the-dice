d9419846f34a78c10a8591211f0c5b3f6f75aaad
pmvroque15 <pmvroque@gmail.com>
2026-04-17 09:18:06 -0400

    Made it better. Added constructor and cleaned main()

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,40 +1,51 @@
 package com.pluralsight;
 
 public class Dice {
-    static void main(String[] args) {
-        //Declare variables
-        int roll1 = 0;
-        int roll2 = 0;
+    public int roll1;
+    public int roll2;
 
-        int twoCounter = 0;
-        int fourCounter = 0;
-        int sixCounter = 0;
-        int sevenCounter = 0;
+    public int twoCounter;
+    public int fourCounter;
+    public int sixCounter;
+    public int sevenCounter;
+
+    int roll() {
+        return (int) ((Math.random() * 6) + 1);
+    }
+
+
+    public Dice() {
+        this.roll1 = roll1;
+        this.roll2 = roll2;
+        this.twoCounter = twoCounter;
+        this.fourCounter = fourCounter;
+        this.sixCounter = sixCounter;
+        this.sevenCounter = sevenCounter;
+    }
+
+    static void main(String[] args) {
+
         //Instance of dice
         Dice dice = new Dice();
 
         //loops 100 times
         for (int i = 0; i < 100; i++) {
-            roll1 = dice.roll();
-            roll2 = dice.roll();
-            int sumOfTwoRolls = (roll1 + roll2);
-            System.out.print("Roll 1: " + roll1 + " - " + roll2 + " Sum: " + sumOfTwoRolls + "\n");
+            dice.roll1 = dice.roll();
+            dice.roll2 = dice.roll();
+            int sumOfTwoRolls = (dice.roll1 + dice.roll2);
+            System.out.print("Roll 1: " + dice.roll1 + " - " + dice.roll2 + " Sum: " + sumOfTwoRolls + "\n");
 
             //if the sum of roll1 and roll2 is 2, and if so, increment the twoCounter
             if (sumOfTwoRolls == 2) {
-                twoCounter++;
+                dice.twoCounter++;
             } else if (sumOfTwoRolls == 4) {
-                fourCounter++;
+                dice.fourCounter++;
             } else if (sumOfTwoRolls == 6) {
-                sixCounter++;
+                dice.sixCounter++;
             } else if (sumOfTwoRolls == 7) {
-                sevenCounter++;
+                dice.sevenCounter++;
             }
         }
-        System.out.printf("TwoCounter: %s\nFourCounter: %s\nSixCounter: %s\nSevenCounter: %s\n", twoCounter,fourCounter,sixCounter,sevenCounter);
-    }
-    //method roll()
-    static int roll() {
-        return (int) ((Math.random() * 6) + 1);
+        System.out.printf("TwoCounter: %s\nFourCounter: %s\nSixCounter: %s\nSevenCounter: %s\n", dice.twoCounter, dice.fourCounter, dice.sixCounter, dice.sevenCounter);
     }
 }
